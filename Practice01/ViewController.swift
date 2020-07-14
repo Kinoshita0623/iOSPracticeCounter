66e689d5ce1f2e22673aad8197220762c5818c3f
kinoshita0623 <kuutarousii@gmail.com>
2020-07-14 10:55:16 +0900

    Initial Commit

---
+++ Practice01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice01
+//
+//  Created by 木下雄一朗 on 2020/07/14.
+//  Copyright © 2020 Panta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
